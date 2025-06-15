333f751f14cf727c907950c3b3463cb40c5f10cb
defa1012 <daniedefa.ddf@gmail.com>
2025-06-15 13:54:12 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = nextConfig;
 import '../styles/globals.css';
 import 'https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css';
 
